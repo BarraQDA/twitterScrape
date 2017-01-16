5a88c50240ea05ca9cc988c8f4f7a8a3d674c765
Jonathan Schultz <jonathan@imatix.com>
2017-01-16 12:22:56 +1100

    Small improvements

--- twitterValidate.py
+++ twitterValidate.py
@@ -25,6 +25,7 @@ from dateutil import parser as dateparser
 parser = argparse.ArgumentParser(description='Validate twitter feed CSV.')
 
 parser.add_argument('-v', '--verbosity', type=int, default=1)
+parser.add_argument('-t', '--threshold', type=int, default=60, help='Number of seconds out of sequence to report.')
 
 parser.add_argument('infile', type=str, help='Input CSV file, if missing use stdin.')
 
@@ -41,21 +42,21 @@ lastdate = None
 blankrowcount = 0
 for row in inreader:
     curid = row['id']
-    curdate = dateparser.parse(row['date'])
     if curid == '':
         blankrowcount += 1
     else:
+        curdate = dateparser.parse(row['date'])
         if blankrowcount > 1:
             print("Multiple blank rows after id:" + (lastid or '') + " - " + (lastdate or ''), file=sys.stderr)
         blankrowcount = 0
         if lastid is not None and curid >= lastid:
-            print("Non-decreasing id after id:" + lastid + " - " + lastdate.isoformat(), file=sys.stderr)
+            print("Non-decreasing id at id:" + curid + " - " + curdate.isoformat(), file=sys.stderr)
         if lastdate is not None:
             offset = (curdate - lastdate).total_seconds()
-            if offset > 0:
-                print("Increasing date after id:" + lastid + " - " + str(offset), file=sys.stderr)
+            if offset > (args.threshold or 0):
+                print("Increasing date at id:" + curid + " - " + str(offset), file=sys.stderr)
             elif offset <= -3600:
-                print("Gap of > 1 hour after id:" + lastid + " - " + str(offset), file=sys.stderr)
+                print("Gap of > 1 hour at id:" + curid + " - " + str(offset), file=sys.stderr)
 
         lastid = curid
         lastdate = curdate
