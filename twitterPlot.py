d58d35a017171dcc88356d1815432bfaaf416ad6
Jonathan Schultz <jonathan@imatix.com>
2017-03-05 09:44:58 +1100

    Multiple plots

--- twitterPlot.py
+++ twitterPlot.py
@@ -22,15 +22,26 @@ import sys
 import unicodecsv
 import datetime
 from dateutil import parser as dateparser
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
 
 parser = argparse.ArgumentParser(description='Twitter frequency matrix plotter.')
 
 parser.add_argument('-v', '--verbosity',  type=int, default=1)
 
+parser.add_argument(      '--since',      type=str, help='Lower bound tweet date.')
+parser.add_argument(      '--until',      type=str, help='Upper bound tweet date.')
+
 parser.add_argument('infile', type=str, nargs='?', help='Input CSV file, otherwise use stdin.')
 
 args = parser.parse_args()
+
+# Parse since and until dates
+if args.until:
+    args.until = dateparser.parse(args.until).date().isoformat()
+if args.since:
+    args.since = dateparser.parse(args.since).date().isoformat()
 
 if args.infile is None:
     infile = sys.stdin
@@ -57,14 +68,40 @@ while True:
     except StopIteration:
         break
 
+    if args.until and row[0] >= args.until:
+        continue
+    if args.since and row[0] < args.since:
+        break
+
     dates.append(dateparser.parse(row[0]))
     for valueidx in range(len(fieldnames) - 1):
         values[valueidx].append(row[valueidx+1])
 
-args = ()
-for valueidx in range(len(fieldnames) - 1):
-    args += (dates, values[valueidx], '')
+#args = ()
+#for valueidx in range(len(fieldnames) - 1):
+    #args += (dates, values[valueidx], '')
 
-plt.plot(*args)
-plt.gcf().autofmt_xdate()
+#plt.plot(*args)
+#plt.gcf().autofmt_xdate()
+#plt.show()
+
+mpl.style.use('classic')
+
+fig, ax1 = plt.subplots()
+ax1.plot(dates, values[0], 'C0')
+ax1.set_xlabel('Date')
+ax1.set_ylabel(fieldnames[1], color='C0')
+ax1.tick_params('n', colors='C0')
+ax1.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+for valueidx in range(1, len(fieldnames) - 1):
+    color = 'C' + str(valueidx)
+    print(color)
+    ax = ax1.twinx()
+    ax.plot(dates, values[valueidx], color)
+    ax.set_ylabel(fieldnames[valueidx+1], color=color)
+    ax.tick_params('n', colors=color)
+    ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+
+fig.tight_layout()
 plt.show()
