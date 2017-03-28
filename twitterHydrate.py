69242f855b4260acfac6889566e9c87e5acb8189
Jonathan Schultz <jonathan@imatix.com>
2017-03-28 20:18:05 +1100

    Changed twitter API library to python-twitter

--- twitterHydrate.py
+++ twitterHydrate.py
@@ -18,16 +18,15 @@
 
 from __future__ import print_function
 import argparse
+from requests_oauthlib import OAuth1Session
+import webbrowser
+import twitter
 import sys
 from TwitterFeed import TwitterRead, TwitterWrite
 import unicodecsv
-import rest
 import re
 from dateutil import parser as dateparser
 import datetime
-import logging
-
-logging.basicConfig(level=logging.INFO)
 
 MENTIONREGEXP=re.compile(r'(@\w+)', re.UNICODE)
 HASHTAGREGEXP=re.compile(r'(#\w+)', re.UNICODE)
@@ -38,8 +37,20 @@ def twitterHydrate(arglist):
                                      fromfile_prefix_chars='@')
 
     parser.add_argument('-v', '--verbosity', type=int, default=1)
-    parser.add_argument('-t', '--threshold', type=int, default=60, help='Number of seconds out of sequence to report.')
 
+    # Twitter authentication stuff
+    parser.add_argument('--consumer-key', type=str,
+                        help='Consumer key for Twitter authentication')
+    parser.add_argument('--consumer-secret', type=str,
+                        help='Consumer secret for Twitter authentication')
+
+    parser.add_argument('-a', '--application-only-auth', action='store_true')
+    parser.add_argument('--access-token-key', type=str,
+                        help='Access token key for Twitter authentication')
+    parser.add_argument('--access-token-secret', type=str,
+                        help='Access token secret for Twitter authentication')
+
+    parser.add_argument('-i', '--ids',      type=str, help='Comma-separated list of ids to retrieve.')
     parser.add_argument('-l', '--limit',      type=int, help='Limit number of tweets to process')
 
     parser.add_argument('-o', '--outfile', type=str, help='Output CSV file, otherwise use stdout')
@@ -66,25 +77,86 @@ def twitterHydrate(arglist):
 
         comments += twitterread.comments
 
+    # Twitter URLs
+    REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+    ACCESS_TOKEN_URL = 'https://api.twitter.com/oauth/access_token'
+    AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
+    SIGNIN_URL = 'https://api.twitter.com/oauth/authenticate'
+
+    if not all([args.consumer_key, args.consumer_secret]):
+        print ("""
+    To access Twitter's API, you need a consumer key and secret for a registered
+    Twitter application. You can register an application or retrieve the consumer key
+    and secret for an already registerd application at https://dev.twitter.com/apps
+    """)
+        sys.exit()
+
+    if args.application_only_auth:
+        api = twitter.Api(
+                    consumer_key=args.consumer_key,
+                    consumer_secret=args.consumer_secret,
+                    application_only_auth=True
+            )
+    else:
+        if not all([args.access_token_key, args.access_token_secret]):
+            oauth_client = OAuth1Session(args.consumer_key, client_secret=args.consumer_secret, callback_uri='oob')
+
+            resp = oauth_client.fetch_request_token(REQUEST_TOKEN_URL)
+            url = oauth_client.authorization_url(AUTHORIZATION_URL)
+
+            print('Opening browser for Twitter authentication: ' + url, file=sys.stderr)
+
+            webbrowser.open(url)
+            print('Enter your pincode? ', file=sys.stderr)
+            pincode = raw_input()
+
+            oauth_client = OAuth1Session(args.consumer_key, client_secret=args.consumer_secret,
+                                        resource_owner_key=resp.get('oauth_token'),
+                                        resource_owner_secret=resp.get('oauth_token_secret'),
+                                        verifier=pincode)
+            resp = oauth_client.fetch_access_token(ACCESS_TOKEN_URL)
+            args.access_token_key = resp.get('oauth_token')
+            args.access_token_secret = resp.get('oauth_token_secret')
+
+            print('To re-use access token next time use the following arguments:', file=sys.stderr)
+            print('    --access-token-key ' + args.access_token_key + ' --access-token-secret ' + args.access_token_secret, file=sys.stderr)
+
+        api = twitter.Api(
+                    consumer_key=args.consumer_key,
+                    consumer_secret=args.consumer_secret,
+                    access_token_key=args.access_token_key,
+                    access_token_secret=args.access_token_secret,
+                    sleep_on_rate_limit=True
+            )
+
     twitterwrite = TwitterWrite(args.outfile, comments=comments, fieldnames = ['user', 'date', 'text', 'replies', 'retweets', 'favorites', 'reply-to', 'reply-to-user', 'reply-to-user-id', 'lang', 'geo', 'mentions', 'hashtags', 'user-id', 'id'])
 
-    ids_to_fetch = sorted([row['id'] for row in twitterread], reverse=True)
+    ids = sorted([row['id'] for row in twitterread], reverse=True)
+    while len(ids):
+        #tweets = [api.GetStatus(ids[0])]
+        #ids = ids[1:]
+        tweets = sorted(api.GetStatuses(ids[0:100]),
+                        key=lambda tweet: tweet.id,
+                        reverse=True)
+        ids = ids[100:]
+        for tweet in tweets:
+            twitterwrite.write({
+                'user': tweet.user.screen_name,
+                'date': datetime.datetime.utcfromtimestamp(tweet.created_at_in_seconds).isoformat(),
+                'text': tweet.text,
+                'reply-to': tweet.in_reply_to_status_id,
+                'reply-to-user': tweet.in_reply_to_screen_name,
+                'reply-to-user-id': tweet.in_reply_to_user_id,
+                'retweets': tweet.retweet_count,
+                'favorites': tweet.favorite_count,
+                'lang': tweet.lang,
+                'geo' : tweet.geo,
+                'mentions': u' '.join([u'@'+user.screen_name for user    in tweet.user_mentions]),
+                'hashtags': u' '.join([u'#'+hashtag.text     for hashtag in tweet.hashtags]),
+                'user-id': tweet.user.id,
+                'id': tweet.id_str,
+            })
 
-    for page in rest.fetch_tweet_list(ids_to_fetch):
-        page = sorted(page, reverse=True, key=lambda item: item['id'])
-        for tweet in page:
-            tweet['user-id']           = tweet['user']['id']
-            tweet['user']              = tweet['user']['screen_name']
-            tweet['date']              = dateparser.parse(tweet['created_at']).replace(tzinfo=None).isoformat()
-            tweet['retweets']          = tweet['retweet_count']
-            tweet['favorites']         = tweet['favorite_count']
-            tweet['reply-to']          = tweet['in_reply_to_status_id']
-            tweet['reply-to-user']     = tweet['in_reply_to_screen_name']
-            tweet['reply-to-user-id']  = tweet['in_reply_to_user_id']
-            tweet['mentions']  = " ".join(MENTIONREGEXP.findall(tweet['text']))
-            tweet['hashtags']  = " ".join(HASHTAGREGEXP.findall(tweet['text']))
-
-            twitterwrite.write(tweet)
 
 if __name__ == '__main__':
     twitterHydrate(None)
