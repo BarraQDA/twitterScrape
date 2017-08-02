f293005ef62eaae0bc94c0c3a740078ab4f7dc28
Jonathan Schultz <jonathan@schultz.la>
2017-08-02 23:16:38 +1000

    Brought twitterReplay into twitterGui fold

--- twitterReplay.py
+++ twitterReplay.py
@@ -17,6 +17,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from __future__ import print_function
+import gooey
 import argparse
 import os
 import sys
@@ -25,27 +26,47 @@ import re
 from datetime import datetime
 import subprocess
 
-def twitterReplay(arglist):
+def add_arguments(parser):
+    parser.description = "Replay twitter file processing"
 
-    parser = argparse.ArgumentParser(description='Replay twitter file processing.',
-                                     fromfile_prefix_chars='@')
+    replaygroup = parser.add_argument_group('Replay')
+    replaygroup.add_argument('input_file', type=str, nargs='*', widget='FileChooser',
+                             help='CSV files to replay.')
+    replaygroup.add_argument('-f', '--force',   action='store_true',
+                             help='Replay even if infile is not older than its dependents.')
+    replaygroup.add_argument(      '--dry-run', action='store_true',
+                             help='Print but do not execute command')
 
-    parser.add_argument('-v', '--verbosity', type=int, default=1)
-    parser.add_argument('-d', '--depth',     type=int, default=1, help='Depth of command history to replay.')
-    parser.add_argument('-f', '--force',     action='store_true', help='Replay command even if infile is not more recent.')
-    parser.add_argument('--dry-run',         action='store_true', help='Print but do not execute command')
+    advancedgroup = parser.add_argument_group('Advanced')
+    advancedgroup.add_argument('-v', '--verbosity', type=int, default=1)
+    advancedgroup.add_argument('-d', '--depth',     type=int, default=1,
+                               help='Depth of command history to replay.')
 
-    parser.add_argument('infile',  type=str, nargs='*', help='Input CSV files with comments to replay.')
+    parser.set_defaults(func=twitterReplay)
 
-    args, extraargs = parser.parse_known_args()
+@gooey.Gooey(ignore_command=None, force_command='--gui',
+             default_cols=1,
+             load_cmd_args=True, use_argparse_groups=True, use_tabs=True)
+def parse_arguments():
+    parser = gooey.GooeyParser()
+    add_arguments(parser)
+    #args, extraargs = parser.parse_known_args()
+    #args.extraargs = extraargs
+    args = parser.parse_args()
+    args.extraargs = []
+    return vars(args)
 
+def build_comments(kwargs):
+    return ''
+
+def twitterReplay(input_file, verbosity, depth, force, dry_run, extraargs, **dummy):
     fileregexp = re.compile(r"^#+ (?P<file>.+) #+$", re.UNICODE)
     cmdregexp  = re.compile(r"^#\s+(?P<cmd>[\w\.-]+)", re.UNICODE)
     argregexp  = re.compile(r"^#\s+(?:--)?(?P<name>[\w-]+)(?:=(?P<quote>\"?)(?P<value>.+)(?P=quote))?", re.UNICODE)
     piperegexp = re.compile(r"^#+$", re.UNICODE)
 
-    for infilename in args.infile:
-        if args.verbosity >= 1:
+    for infilename in input_file:
+        if verbosity >= 1:
             print("Replaying " + infilename, file=sys.stderr)
 
         # Read comments at start of infile.
@@ -104,14 +125,14 @@ def twitterReplay(arglist):
                     commentline = comments.pop(0) if len(comments) > 0 else None
                     argmatch = argregexp.match(commentline) if commentline else None
 
-                pipestack.append((cmd, arglist + extraargs + ['--verbosity', str(args.verbosity)]))
+                pipestack.append((cmd, arglist + extraargs + ['--verbosity', str(verbosity)]))
                 pipematch = piperegexp.match(commentline) if commentline else None
 
             if outfile:
                 replaystack.append((pipestack, infilelist, outfile))
 
             depth += 1
-            if depth == args.depth:
+            if depth == depth:
                 break
 
             filematch = fileregexp.match(commentline) if commentline else None
@@ -122,10 +143,12 @@ def twitterReplay(arglist):
         else:
             pipestack = None
 
-        execute = args.force
+        execute = force
         while pipestack:
             if not execute:
                 outfilestamp = (datetime.utcfromtimestamp(os.path.getmtime(outfilename)) if os.path.isfile(outfilename) else None) if outfilename else None
+                if not infilelist:
+                    execute = True
                 for infilename in infilelist:
                     infilestamp = (datetime.utcfromtimestamp(os.path.getmtime(infilename)) if os.path.isfile(infilename) else None) if infilename else None
                     if infilestamp > (outfilestamp or datetime.min):
@@ -141,10 +164,10 @@ def twitterReplay(arglist):
                     infilelist = None
                     if len(pipestack) == 0 and '--outfile' not in arglist:
                         arglist = arglist + ['--outfile', outfilename]
-                    if args.verbosity >= 1:
+                    if verbosity >= 1:
                         print("Executing: " + cmd + ' ' + ' '.join(arglist), file=sys.stderr)
 
-                    if not args.dry_run:
+                    if not dry_run:
                         if not process:
                             process = subprocess.Popen([cmd] + arglist,
                                                        stdout=subprocess.PIPE if len(pipestack) else sys.stdout,
@@ -154,10 +177,10 @@ def twitterReplay(arglist):
                                                        stdout=subprocess.PIPE if len(pipestack) else sys.stdout,
                                                        stdin=process.stdout,
                                                        stderr=sys.stderr)
-                if not args.dry_run:
+                if not dry_run:
                     process.wait()
             else:
-                if args.verbosity >= 2:
+                if verbosity >= 2:
                     print("File not replayed: " + outfilename, file=sys.stderr)
 
             if replaystack:
@@ -166,4 +189,5 @@ def twitterReplay(arglist):
                 pipestack = None
 
 if __name__ == '__main__':
-    twitterReplay(None)
+    kwargs = parse_arguments()
+    kwargs['func'](**kwargs)
